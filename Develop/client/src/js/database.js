f9da04260a68faf8d32a17f56817439b742808cd
WarrenAdam <130782045+WarrenAdam@users.noreply.github.com>
2023-10-06 17:24:02 -0400

    added getDb logic

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Added logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('Post to the database');
 
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
-  // Use the .add() method on the store and pass in the content.
+  // Use the .update() method on the store and pass in the content.
   const request = store.update({content});
 
   // Get confirmation of the request.
@@ -34,7 +34,28 @@ export const putDb = async (content) => {
   
   console.error('putDb not implemented')};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
 
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  console.error('getDb not implemented')
+  return result;
+};
+  
+// Start the database.
 initdb();
